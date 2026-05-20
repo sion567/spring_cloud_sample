bddb4e290f7dfd163255d7d60e413bd4d327e315
huanggang <huanggang@zdsoft.cn>
2026-05-20 16:21:54 +0800

    commit

---
+++ shop-parent/gateway/src/main/java/com/shop/gateway/filter/GatewayLogFilter.java
@@ -0,0 +1,38 @@
+package com.shop.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Component
+@Slf4j
+public class GatewayLogFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String rawPath = exchange.getRequest().getURI().getPath();
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            URI routeUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+            Long statusCode = exchange.getResponse().getStatusCode() != null ?
+                    (long) exchange.getResponse().getStatusCode().value() : 0L;
+
+            if (routeUri != null) {
+                log.info("网关监控 -> 原始请求: [{}], 最终实际转发目的地: [{}], 响应状态码: [{}]",
+                        rawPath, routeUri, statusCode);
+            }
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+}
---
+++ shop-parent/gateway/src/main/java/com/shop/gateway/filter/GatewayLogGatewayFilterFactory.java
@@ -0,0 +1,29 @@
+package com.shop.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Component
+@Slf4j
+public class GatewayLogGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String rawPath = exchange.getRequest().getURI().getPath();
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                URI routeUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+                Long statusCode = exchange.getResponse().getStatusCode() != null ?
+                        (long) exchange.getResponse().getStatusCode().value() : 0L;
+                if (routeUri != null) {
+                    log.info("----路由监控 -> 原始请求: [{}], 最终实际转发目的地: [{}], 响应状态码: [{}]", rawPath, routeUri, statusCode);
+                }
+            }));
+        };
+    }
+}
