1412ada11183a17529b0704fc1b3d7e064945722
huanggang <huanggang@zdsoft.cn>
2026-05-26 19:07:44 +0800

    commit

--- shop-parent/gateway/src/main/java/com/shop/gateway/GatewayApplication.java
+++ shop-parent/gateway/src/main/java/com/shop/gateway/GatewayApplication.java
@@ -4,15 +4,22 @@ import com.shop.gateway.filter.GatewayLogFilter;
 import com.shop.gateway.filter.GatewayLogFilter2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@LoadBalancerClients({
+        @LoadBalancerClient("service-gateway-provider")
+})
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
